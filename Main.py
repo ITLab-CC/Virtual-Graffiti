Fix scaling

Main.py
@@ -321,17 +321,17 @@ while Running:
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(blur)
     if DEBUG == True: # Draw the keypoints in image
+        img = cv2.flip(img, 1) # Mirror image
         blank = np.zeros((1, 1))
         blobs = cv2.drawKeypoints(img, keypoints, blank, (0, 0, 255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     coordinates = cv2.KeyPoint_convert(keypoints) # convert keypoints to coordinates
     # For each blob 
     for p in coordinates:
-        x = int(p[0])
-        y = int(p[1])
+        x = SCREEN_X-int(p[0])-1
+        y = SCREEN_Y-int(p[1])-1
         if DEBUG == True: # Write cordinates to the blob in the image
-            blobs = cv2.flip(blobs, 1) # Mirror image
-            text = str(x) + "|" + str(y)
+            text = str(x*SCALE_FACTOR_X) + "|" + str(y*SCALE_FACTOR_Y)
             blobs = cv2.putText(blobs, text, (x+10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1, cv2.LINE_AA)
             
 
@@ -340,8 +340,7 @@ while Running:
             Calibrate_Points(x, y)
         else:
             # Move mouse cursor to position
-            # mouse.move(x*SCALE_FACTOR_X, y* SCALE_FACTOR_Y)
-            mouse.move(SCREEN_X-(x*SCALE_FACTOR_X) -1, SCREEN_Y-(y* SCALE_FACTOR_Y) -1)
+            mouse.move(x*SCALE_FACTOR_X, y*SCALE_FACTOR_Y)
             if(MOUSE_PRESSED == 0): # Press mouse button if mouse is not pressed
                 #print("press")
                 mouse.press(button='left')