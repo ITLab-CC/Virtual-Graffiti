c37cfc19b60a648bb21b6375afebd330a7b0eeed
Calkstein <lucas.bischoff@freenet.de>
2022-12-07 12:20:19 +0100

    :construction: optimise speed

--- module/paint.py
+++ module/paint.py
@@ -1,6 +1,7 @@
 import math                  # For calculating sqrt
 import random                # Random number
 from tkinter import *        # Drawing
+# from PIL import Image, ImageTk 
 
 # Paint
 class Paint:
@@ -11,7 +12,7 @@ class Paint:
     Name = "PAINT SCREEN"
     SPRAY_COLOUR = "#00FF00"
     
-    def __init__(self, spray_color="#00FF00", enabled=True):
+    def __init__(self, spray_color="#00FF00", visible=True):
         self.SPRAY_COLOUR = spray_color
         # Screen init
         self.tkScreen = Tk()
@@ -19,19 +20,18 @@ class Paint:
         self.tkScreen.title(self.Name)
         self.canvas = Canvas(self.tkScreen, width=self.tkScreen.winfo_screenwidth(), height=self.tkScreen.winfo_screenheight(), bg="white")
         self.canvas.pack()
-        if enabled:
+        if visible:
             self.Show()
         else:
             self.Hide()
-        self.tkScreen.update()
     
     def Hide(self):
         self.tkScreen.withdraw() # Hide the screen
-        self.tkScreen.update()
+        self.Screen_Update()
         
     def Show(self):
         self.tkScreen.deiconify() # Show the screen
-        self.tkScreen.update()
+        self.Screen_Update()
         
     def End(self):
         self.tkScreen.destroy()
@@ -57,7 +57,7 @@ class Paint:
             newY = random.randint(-currYVerteilung, currYVerteilung) + newY
             # print("Abstand", abstandFromTo, "RandG", str(randG0To1), "NewX", str(newX), "NewY", str(newY))
             self.canvas.create_oval(newX, newY, newX+2, newY+2, fill=self.SPRAY_COLOUR, outline=self.SPRAY_COLOUR)
-        self.tkScreen.update()
+        # self.tkScreen.update()
         # canvas.create_line(fromX, from.Y, toX, toY, fill="red", width=5)
 
     def createGrafittiLineBigger(self, fromX, fromY, toX, toY, blobSize):
@@ -73,5 +73,30 @@ class Paint:
             newY = random.randint(-currYVerteilung, currYVerteilung) + newY
             # print("Abstand", abstandFromTo, "RandG", str(randG0To1), "NewX", str(newX), "NewY", str(newY))
             self.canvas.create_oval(newX, newY, newX+2, newY+2, fill=self.SPRAY_COLOUR, outline=self.SPRAY_COLOUR)
-        self.tkScreen.update()
+        # self.tkScreen.update()
         # canvas.create_line(fromX, from.Y, toX, toY, fill="red", width=5)
+        
+    def Screen_Update(self):
+        self.tkScreen.update()
+        
+    # def create_rectangle(self, x,y,a,b,**options):
+    #     images=[]
+    #     if 'alpha' in options:
+    #         # Calculate the alpha transparency for every color(RGB)
+    #         alpha = int(options.pop('alpha') * 255)
+    #         # Use the fill variable to fill the shape with transparent color
+    #         fill = options.pop('fill')
+    #         fill = self.tkScreen.winfo_rgb(fill) + (alpha,)
+    #         image = Image.new('RGBA', (a-x, b-y), fill)
+    #         images.append(ImageTk.PhotoImage(image))
+    #         self.canvas.create_image(x, y, image=images[-1], anchor='nw')
+    #         self.canvas.create_rectangle(x, y,a,b, **options)
+    
+    # def Draw(self, x, y, size):
+    #     x = int(x)
+    #     y = int(y)
+    #     # self.canvas.create_oval(x, y, x+(size*2), y+(size*2), fill=self.SPRAY_COLOUR, outline=self.SPRAY_COLOUR)
+    #     # self.create_rectangle(x, y, int(x+(size*2)), int(y+(size*2)), fill="blue", alpha=.3)
+    #     self.create_rectangle(50, 110,300,280, fill=self.SPRAY_COLOUR, alpha=.3)
+        
+    #     self.Screen_Update()
